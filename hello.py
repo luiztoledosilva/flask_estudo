01b6934de9fb6ac4d1b39f61a88c4c5d18243561
luiztoledosilva <89946718+luiztoledosilva@users.noreply.github.com>
2022-10-27 20:47:13 -0300

    modificacoes2

--- hello.py
+++ hello.py
@@ -24,6 +24,9 @@ def get_post(post_id):
 
 
 
+
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
@@ -35,6 +38,19 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route('/create', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                         (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index.html'))
     return render_template('create.html')
 def post(post_id):
     post = get_post(post_id)
